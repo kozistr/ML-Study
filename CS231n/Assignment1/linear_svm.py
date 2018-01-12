67b914239d07af8baea24714fe8b039da584266b
kozistr <kozistr@gmail.com>
2018-01-12 21:34:25 +0900

    change some stuffs...

--- CS231n/Assignment1/linear_svm.py
+++ CS231n/Assignment1/linear_svm.py
@@ -23,18 +23,18 @@ def svm_loss_naive(w, x, y, reg):
         scores = w.dot(x[:, i])
         correct_scores = scores[y[i]]
 
-        indicator = (scores - correct_scores + 1) > 0.
-
         for j in range(n_classes):
             if j == y[i]:
-                dw[j, :] = -np.sum(np.delete(indicator, j)) * x[:, i].T
                 continue
 
-            dw[j, :] += indicator[j] * x[:, i].T
             margin = scores[j] - correct_scores + 1.
 
             if margin > 0.:
                 loss += margin
+
+                # update gradient
+                dw[j, :] += x[:, i].T
+                dw[y[i], :] -= x[:, i].T
 
     loss /= n_train
     dw /= n_train
@@ -61,19 +61,26 @@ def svm_loss_vectorized(w, x, y, reg):
 
     n_train = x.shape[1]
 
-    loss = w.dot(x) - (w.dot(x))[y, np.arange(n_train)] + 1.
-    indicator = loss > 0.
+    scores = w.dot(x)
+    correct_scores = np.ones(scores.shape) * scores[y, np.arange(n_train)]
+    deltas = np.ones(scores.shape)
 
-    loss = np.sum(loss * indicator, axis=0) - 1.
+    margin = scores - correct_scores + deltas
+
+    indicator = margin > 0.
+
+    margin[margin < 0] = 0.                     # min value, 0
+    margin[y, np.arange(scores.shape[1])] = 0.  # case of i == y_i
+    # margin = np.sum(margin * indicator, axis=0) - 1.
 
     # L2 regularization
     regularization = .5 * reg * np.sum(np.power(w, 2))
-    loss = np.sum(loss) / float(n_train) + regularization
 
-    indicator *= np.ones(loss.shape)
+    loss = np.sum(margin) / float(n_train) + regularization
+
+    indicator *= np.ones(margin.shape)
     indicator[[y, np.arange(n_train)]] = -(np.sum(indicator, axis=0) - 1.)
 
-    dw = indicator.dot(x.T) / float(n_train)
-    dw += reg * w
+    dw = indicator.dot(x.T) / float(n_train) + reg * w
 
     return loss, dw
