b6cd77766162cea805173925693074599eb58d83
kozistr <kozistr@gmail.com>
2018-01-13 17:43:42 +0900

    update linear classifier

--- CS231n/Assignment1/linear_classifier.py
+++ CS231n/Assignment1/linear_classifier.py
@@ -3,24 +3,24 @@ from linear_svm import *
 from softmax import *
 
 
-class LinearClassifier:
+class LinearClassifier(object):
 
     def __init__(self, batch_size=64, n_iters=1000, log_iters=100,
-                 learning_rate=1e-3, initializer=None, regularization=5e-4):
+                 learning_rate=1e-3, regularization=5e-4):
         """
-        :param batch_size: training batch_size, default 64
-        :param n_iters: the number of iterations, default 1000
-        :param log_iters: the number of logging iterations, default 100
-        :param learning_rate: training learning rate, default 1e-3
-        :param initializer: weight initializer, default None
-        :param regularization: weight regularization, default 5e-4
+        Train this linear classifier using stochastic gradient descent.
+
+        :param batch_size: (integer) number of training examples to use at each step, default 64
+        :param n_iters: (integer) number of steps to take when optimizing, default 1000
+        :param log_iters: (integer) number of steps to log when optimizing, default 100
+        :param learning_rate: (float) learning rate for optimization., default 1e-3
+        :param regularization: (float) regularization strength., default 5e-4
         """
 
         self.batch_size = batch_size
         self.n_iters = n_iters
         self.log_iters = log_iters
         self.lr = learning_rate
-        self.w_init = initializer
         self.reg = regularization
 
         self.x = None  # images
@@ -28,29 +28,62 @@ class LinearClassifier:
         self.n_classes = 0  # the number of classes
 
         self.W = None  # Weights
-        # self.b = None  # biases
 
     def train(self, x, y):
+        """
+        :param x: A numpy array of shape (N, D) containing training data;
+        there are N training samples each of dimension D.
+        :param y: A numpy array of shape (N,) containing training labels;
+        y[i] = c means that X[i] has label 0 <= c < C for C classes.
+        :return: A list containing the value of the loss function at each training iteration.
+        """
+
         dim, n_train = x.shape
-        self.n_classes = np.max(y) + 1
+        self.n_classes = np.max(y) + 1  # assume y takes values 0...K-1 where K is number of classes
 
         if not self.W:
-            self.W = np.random.randn(self.n_classes, dim) * 0.001
+            self.W = np.random.randn(self.n_classes, dim) * 0.001  # lazily initialize W
 
+        # Run stochastic gradient descent to optimize W
         losses = []
         for i in range(self.n_iters):
+            #########################################################################
+            # TODO:                                                                 #
+            # Sample batch_size elements from the training data and their           #
+            # corresponding labels to use in this round of gradient descent.        #
+            # Store the data in X_batch and their corresponding labels in           #
+            # y_batch; after sampling X_batch should have shape (dim, batch_size)   #
+            # and y_batch should have shape (batch_size,)                           #
+            #                                                                       #
+            # Hint: Use np.random.choice to generate indices. Sampling with         #
+            # replacement is faster than sampling without replacement.              #
+            #########################################################################
+
             # Get training batch data
             rand_idx = np.random.choice(n_train, self.batch_size, replace=True)
 
             x_batch = x[:, rand_idx]
             y_batch = y[rand_idx]
 
+            #########################################################################
+            #                       END OF YOUR CODE                                #
+            #########################################################################
+
             # Compute loss & gradient
             loss, gradient = self.loss(x_batch, y_batch, self.reg)
             losses.append(loss)
 
             # Update weights using gradients & learning rate
+            #########################################################################
+            # TODO:                                                                 #
+            # Update the weights using the gradient and the learning rate.          #
+            #########################################################################
+
             self.W += - gradient * self.lr
+
+            #########################################################################
+            #                       END OF YOUR CODE                                #
+            #########################################################################
 
             if i % self.log_iters == 0:
                 print("[+] Iter %04d, loss : {.:8f}".format(i, np.mean(losses)))
@@ -58,22 +91,52 @@ class LinearClassifier:
         return losses
 
     def predict(self, x):
+        """
+        Use the trained weights of this linear classifier to predict labels for data points.
+
+        :param x: A numpy array of shape (N, D) containing training data;
+        there are N training samples each of dimension D.
+        :return: Predicted labels for the data in X. y_pred is a 1-dimensional array of length N,
+        and each element is an integer giving the predicted class.
+        """
+
+        ###########################################################################
+        # TODO:                                                                   #
+        # Implement this method. Store the predicted labels in y_pred.            #
+        ###########################################################################
+
         scores = self.W.dot(x)
         y_prediction = scores.argmax(axis=0)
+
+        ###########################################################################
+        #                           END OF YOUR CODE                              #
+        ###########################################################################
 
         return y_prediction
 
     def loss(self, x_batch, y_batch, reg):
+        """
+        Compute the loss function and its derivative. Subclasses will override this.
+
+        :param x_batch: A numpy array of shape (N, D) containing a minibatch of N data points;
+        each point has dimension D.
+        :param y_batch: A numpy array of shape (N,) containing labels for the mini-batch.
+        :param reg: (float) regularization strength.
+        :return: loss (single float), gradient (an array of same shape as W)
+        """
+
         pass
 
 
 class LinearSVM(LinearClassifier):
+    """ A subclass that uses the Multiclass SVM loss function """
 
     def loss(self, x_batch, y_batch, reg):
         return svm_loss_vectorized(self.W, x_batch, y_batch, reg)
 
 
 class Softmax(LinearClassifier):
+    """ A subclass that uses the Softmax + Cross-entropy loss function """
 
     def loss(self, x_batch, y_batch, reg):
         return softmax_loss_vectorized(self.W, x_batch, y_batch, reg)
