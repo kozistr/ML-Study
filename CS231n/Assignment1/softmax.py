34291f5c9a68571dda8b9e70c33f4b5541fa33c7
kozistr <kozistr@gmail.com>
2018-01-13 17:31:32 +0900

    update Softmax

--- CS231n/Assignment1/softmax.py
+++ CS231n/Assignment1/softmax.py
@@ -3,15 +3,26 @@ import numpy as np
 
 def softmax_loss_naive(w, x, y, reg):
     """
-    softmax loss function, naive implementation
-    :param w: weights
-    :param x: x images
-    :param y: y labels
-    :param reg: weights regularization
-    :return: loss, gradient
+    Softmax loss function, naive implementation (with loops)
+
+    Inputs have dimension D, there are C classes, and we operate on mini-batches of N examples.
+
+    :param w: A numpy array of shape (D, C) containing weights.
+    :param x: A numpy array of shape (N, D) containing a mini-batch of data.
+    :param y: A numpy array of shape (N,) containing training labels;
+    y[i] = c means that X[i] has label c, where 0 <= c < C.
+    :param reg: (float) regularization strength
+    :return: loss (single float), gradient (an array of same shape as W)
     """
 
     dw = np.zeros(w.shape)
+
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
 
     n_train = x.shape[1]
     n_classes = dw.shape[0]
@@ -38,28 +49,43 @@ def softmax_loss_naive(w, x, y, reg):
         loss += -np.log(numerator / float(exp_score_sum_i))
 
     # L2 regularization
-    regularization = .5 * reg * np.sum(np.power(w, 2))
+    w_reg = .5 * reg * np.sum(np.power(w, 2))
+    dw_reg = reg * w  # differential of w_reg by w
 
-    loss = loss / float(n_train) + regularization
-    dw = dw / float(n_train) + reg * w
+    loss = loss / float(n_train) + w_reg
+    dw = dw / float(n_train) + dw_reg
+
+    #############################################################################
+    #                          END OF YOUR CODE                                 #
+    #############################################################################
 
     return loss, dw
 
 
 def softmax_loss_vectorized(w, x, y, reg):
     """
-    softmax loss function, vectorized implementation
-    :param w: weights
-    :param x: x images
-    :param y: y labels
-    :param reg: weights regularization
-    :return: loss, gradient
+    Softmax loss function, vectorized version.
+
+    Inputs and outputs are the same as softmax_loss_naive.
+
+    :param w: Same as above
+    :param x: Same as above
+    :param y: Same as above
+    :param reg: Same as above
+    :return: Same as above
     """
 
     # dw = np.zeros(w.shape)
 
     n_train = x.shape[1]
     # n_classes = dw.shape[0]
+
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
 
     score = w.dot(x)
     stability = -np.max(score, axis=0)
@@ -69,13 +95,18 @@ def softmax_loss_vectorized(w, x, y, reg):
     exp_score_sum = np.sum(exp_score, axis=0)
 
     # L2 regularization
-    regularization = .5 * reg * np.sum(np.power(w, 2))
+    w_reg = .5 * reg * np.sum(np.power(w, 2))
+    dw_reg = reg * w  # differential of w_reg by w
 
     loss = np.log(exp_score_sum) - score[y, np.arange(n_train)]
-    loss = np.sum(loss) / float(n_train) + regularization
+    loss = np.sum(loss) / float(n_train) + w_reg
 
     dw = exp_score / exp_score_sum
     dw[y, np.arange(n_train)] += -1.
-    dw = dw.dot(x.T) / float(n_train) + reg * w
+    dw = dw.dot(x.T) / float(n_train) + dw_reg
+
+    #############################################################################
+    #                          END OF YOUR CODE                                 #
+    #############################################################################
 
     return loss, dw
